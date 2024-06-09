c8039d1217ef4675b9c0bddf5119eeaf6779694d
ahzqr <a.haaazq@gmail.com>
2024-06-10 05:34:37 +0800

    feat: navigate to feed upon login

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -15,7 +15,7 @@ export default function LoginForm({ setUser }) {
     const { username, password } = data;
     const user = await login(username, password);
     setUser(user);
-    navigate("/orders");
+    navigate("/feed");
   };
 
   return (
