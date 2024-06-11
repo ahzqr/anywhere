190c8332589b72624af90e81311b59c2019785f9
ahzqr <a.haaazq@gmail.com>
2024-06-12 02:43:42 +0800

    feat: update navbar w admin option

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 import * as userService from "../../utilities/users-service";
 
 export default function NavBar({ setUser }) {
+  const user = userService.getUser();
+
   function handleLogOut() {
     userService.logout();
     setUser(null);
   }
   return (
     <nav className="navbar">
+      {user.isAdmin ? <Link to="/admin">Admin</Link> : ""}
+      <Link to="/feed">Home</Link>
+      <Link to={`/profile/${user._id}`}>Profile</Link>
+      <Link to="saved">Saved</Link>
       <Link to="" onClick={handleLogOut} className="logout-link">
         Log Out
       </Link>
