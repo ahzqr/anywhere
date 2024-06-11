c971822719945202575662dfd2a2c4250cc27c9e
ahzqr <a.haaazq@gmail.com>
2024-06-12 02:45:52 +0800

    feat: update following routes and add save content

--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -7,8 +7,10 @@ const ensureLoggedIn = require("../../config/ensureLoggedIn");
 router.post("/", usersCtrl.create);
 router.post("/login", usersCtrl.login);
 router.get("/check-token", [ensureLoggedIn], usersCtrl.checkToken);
-router.post("/:userId/follow", usersCtrl.createFollow);
-router.delete("/:userId/unfollow", usersCtrl.deleteFollow);
+router.get("/:userId/following", [ensureLoggedIn], usersCtrl.getFollowingStatus);
+router.post("/:userId/follow", [ensureLoggedIn], usersCtrl.createFollow);
+router.delete("/:userId/unfollow", [ensureLoggedIn], usersCtrl.deleteFollow);
 router.get("/:userId", usersCtrl.getProfile);
+router.get("/:userId/saved", usersCtrl.getSavedContent);
 
 module.exports = router;
