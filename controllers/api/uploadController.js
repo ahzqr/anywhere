0888500123d4f8c54550d600477b5d16c339edaf
ahzqr <a.haaazq@gmail.com>
2024-06-13 00:42:05 +0800

    feat: add controller to add profile pic

--- controllers/api/uploadController.js
+++ controllers/api/uploadController.js
@@ -29,6 +29,36 @@ const uploadImages = async (req, res) => {
     }
 }
 
+const uploadProfilePic = async (req, res) => {
+    try {
+        const file = req.file;
+        const uploadResult = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream({
+                fetch_format: 'auto',
+                quality: "auto",
+                crop: "fill",
+                width: 150,
+                height: 150,
+                gravity: "center"
+
+            },
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                });
+            stream.end(file.buffer);
+        });
+
+        res.json({ url: uploadResult.secure_url });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    uploadImages
+    uploadImages,
+    uploadProfilePic
 }
