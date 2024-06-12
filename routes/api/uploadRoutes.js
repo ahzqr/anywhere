ecb556905fc8d90649a89513f740a4af067ce718
ahzqr <a.haaazq@gmail.com>
2024-06-13 00:41:45 +0800

    feat: add route to upload profile pic

--- routes/api/uploadRoutes.js
+++ routes/api/uploadRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const uploadCtrl = require("../../controllers/api/uploadController");
 
 router.post("/", uploadCtrl.uploadImages);
+router.post("/profilepic", uploadCtrl.uploadProfilePic);
 
 module.exports = router;
